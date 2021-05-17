907fa13369bf69061772ab70983cb4ea41665f8a
JonoHasMono <fry22331@lcsd2kids.org>
2021-05-17 09:26:54 -0600

    Got the basic movement of the character complete

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const gameVar = document.getElementById("game");
 
-let x = 0
-let y = 0
+let x = 50;
+let i = 0
+let j = 0
+let positionX = x + "%";
+
 
 function createCharacter() {
     let character = document.createElement("div");
     character.setAttribute("id", "character");
     character.classList.add("characterDiv");
-    character.style.left = x;
-    character.style.top = y;
+    character.style.left = positionX;
+    character.style.top = "90%";
     document.body.appendChild(character);
     let charVis = document.createElement("img");
     charVis.setAttribute("src", "images/cat.jpeg");
@@ -16,21 +19,63 @@ function createCharacter() {
     character.appendChild(charVis);
 }
 
+document.addEventListener('keydown', logKey);
+
+
+function keyCheck() {
+        setTimeout(() => { 
+                logKey(e);
+                i++
+                if (i < Infinity) {
+                    keyCheck();
+                }
+        }, 10);
+}
 
 function logKey(e) {
     let key = ` ${e.code}`
     key = key.toString();
-    if (key == ' KeyW') {
-        moveUp();
-      } else if (key == ' KeyA') {
-          moveLeft();
-      } else if (key == ' KeyS') {
-          moveDown();
+    i = 0;
+    j = 0;
+    if (key == ' KeyA') {
+        moveLeft();
       } else if (key == ' KeyD') {
-          moveRight();
-      } else if (key == ' KeyL') {
-          //hi
+        moveRight();
       }
 }
+
+
+function moveLeft() {
+    if(positionX == "2%") {
+        i = 50;
+    } else {
+        setTimeout(() => { 
+            x = x - 1
+            positionX = x + "%";
+            character.style.left = positionX;
+            i++
+            if (i < 10) {
+                moveLeft();
+            }
+        }, 10);
+    }
+}
+
+function moveRight() {
+    if(positionX == "95%") {
+        j = 50;
+    } else {
+        setTimeout(() => { 
+            x = x + 1
+            positionX = x + "%";
+            character.style.left = positionX;
+            j++
+            if (j < 10) {
+                moveRight();
+            }
+        }, 10);
+    }
+}
+
 
 createCharacter();
